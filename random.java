f58dfe3cab3897a978c1826b9c1a16dec41b1c79
rensibhut <141635057+rensibhut@users.noreply.github.com>
2025-03-29 15:05:17 +0530

    Add files via upload

---
+++ random.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+class random
+{
+    public static void main(String s[])
+    {
+        Random r = new Random();
+        System.out.println("Random Value :"+r.nextInt());
+    }
+}
+
+//Random Value :229861623
