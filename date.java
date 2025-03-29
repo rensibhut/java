b3baca4de5d4def99aac38ec1a1b6dcfa3d21261
rensibhut <141635057+rensibhut@users.noreply.github.com>
2025-03-29 15:05:59 +0530

    Add files via upload

---
+++ date.java
@@ -0,0 +1,11 @@
+import java.util.Date;  
+class date
+{
+    public static void main(String p[])
+    {
+        Date date=new Date();
+        System.out.println("Today :- " +date);
+    }
+}
+
+//Today :- Sat Mar 29 15:04:01 IST 2025
